a372edace6637289f4a46b08b0da4ca2bcd855e9
h4farhan <h4farhan@dti-id.com>
2025-05-16 11:00:58 +0700

    ini

--- Dashboard.cs
+++ Dashboard.cs
@@ -18,8 +18,6 @@ namespace GOS_FxApps
             InitializeComponent();
             LoadchartLS();
             LoadchartRB();
-            LoadchartSM();
-            LoadchartUM();
         }
 
         private void LoadchartLS()
