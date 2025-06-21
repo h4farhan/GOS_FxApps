4d93cae80bb0de13d874cc8ac57258f6a9ab4b2d
h4farhan <h4farhan@dti-id.com>
2025-06-21 15:32:29 +0700

    a

--- printpenerimaan.cs
+++ printpenerimaan.cs
@@ -153,7 +153,7 @@ namespace GOS_FxApps
 
         private void guna2Button2_Click(object sender, EventArgs e)
         {
-            formperbaikan();
+            formpenerimaan();
         }
 
         private void printpenerimaan_Load(object sender, EventArgs e)
