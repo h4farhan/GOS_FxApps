9187a40db7d3404ad2996fa219675e7481893b80
Nain181107 <muhammadzulkarnaineffendy@gmail.com>
2025-08-06 13:07:44 +0700

    nih ha

--- koneksi.cs
+++ koneksi.cs
@@ -9,14 +9,14 @@ namespace GOS_FxApps
 {
     public static class Koneksi
     {
-        //private static readonly string connectionString = "Data Source=mssql-200510-0.cloudclusters.net,19998;Initial Catalog=gos_apps;Persist Security Info=True;" +
-        //    "User ID=DTIAdmin;Password=%Tii2025%;Encrypt=True;TrustServerCertificate=True;";
+        private static readonly string connectionString = "Data Source=mssql-200510-0.cloudclusters.net,19998;Initial Catalog=gos_apps;Persist Security Info=True;" +
+            "User ID=DTIAdmin;Password=%Tii2025%;Encrypt=True;TrustServerCertificate=True;";
 
         //private static readonly string connectionString = "Data Source=192.168.1.25,1433;Initial Catalog=gos_apps;Persist Security Info=True;" +
         //    "User ID=sa;Password=%tii2025%;TrustServerCertificate=True;";
 
-        private static readonly string connectionString = "Data Source=TII_NB2_1122;Initial Catalog=gos_apps;Persist Security Info=True;" +
-            "User ID=sa;Password=%tii2025%;TrustServerCertificate=True;";
+        //private static readonly string connectionString = "Data Source=TII_NB2_1122;Initial Catalog=gos_apps;Persist Security Info=True;" +
+        //    "User ID=sa;Password=%tii2025%;TrustServerCertificate=True;";
 
         public static SqlConnection GetConnection()
         {
