03ac5b4fb42465b943e46b38a7247e34057edfa5
Nain181107 <muhammadzulkarnaineffendy@gmail.com>
2025-09-10 23:33:57 +0700

    nah

--- koneksi.cs
+++ koneksi.cs
@@ -13,18 +13,18 @@ namespace GOS_FxApps
     {
         // untuk koneksi lokas
         //Data Source = TII_NB2_1122; Initial Catalog = gos_apps; User ID = tii; Password=mm;TrustServerCertificate=True
-        private static readonly string connectionString = "Data Source=TII_NB2_1122;" +
-            "Initial Catalog=gos_apps;" +
-            "Persist Security Info=True;" +
-            "User ID=tii;Password=mm;" +
-            "TrustServerCertificate=True;";
-
-        //Data Source = 198.168.1.64; Initial Catalog = gos_apps; User ID = sa; Password=$Genta2025$;TrustServerCertificate=True
-        //private static readonly string connectionString = "Data Source=192.168.1.64;" +
+        //private static readonly string connectionString = "Data Source=TII_NB2_1122;" +
         //    "Initial Catalog=gos_apps;" +
         //    "Persist Security Info=True;" +
-        //    "User ID=sa;Password=$Genta2025$;" +
+        //    "User ID=tii;Password=mm;" +
         //    "TrustServerCertificate=True;";
+
+        //Data Source = 198.168.1.64; Initial Catalog = gos_apps; User ID = sa; Password=$Genta2025$;TrustServerCertificate=True
+        private static readonly string connectionString = "Data Source=192.168.1.64;" +
+            "Initial Catalog=gos_apps;" +
+            "Persist Security Info=True;" +
+            "User ID=sa;Password=$Genta2025$;" +
+            "TrustServerCertificate=True;";
 
         public static SqlConnection GetConnection()
         {
